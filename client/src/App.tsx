8614a6acc0b1a100e718e74fc14a9751b716c9d1
Ashirwad jha <ashujha0914@gmail.com>
2026-09-10 15:03:31 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,31 +6,27 @@ import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import { SiteChrome } from "./components/SiteChrome";
 import Landing from "./pages/Landing";
-import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import Skills from "./pages/Skills";
 import Certifications from "./pages/Certifications";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
 import Mainframe from "./pages/Mainframe";
-// ...
-<Route path="/mainframe" element={<Mainframe />} />
 
 function Router() {
   return (
     <Switch>
       <Route path="/" component={Landing} />
-      <Route path="/about" component={Home} />
       <Route path="/projects" component={Projects} />
       <Route path="/skills" component={Skills} />
       <Route path="/certifications" component={Certifications} />
       <Route path="/contact" component={Contact} />
+      <Route path="/mainframe" component={Mainframe} />
       <Route path="/404" component={NotFound} />
       <Route component={NotFound} />
     </Switch>
   );
 }
-
 
 function App() {
   return (
